ec5b6977535955eaa86d99eb343576cc40d91024
Sudeep <sudeeppprajapati@gmail.com>
2026-01-14 14:34:24 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <SignInButton />
+      <UserButton />
+    </>
+  );
+}
