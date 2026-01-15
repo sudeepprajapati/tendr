abf91c36fa0a78ebadbc2808b5207a5fee104904
Sudeep <sudeeppprajapati@gmail.com>
2026-01-15 16:23:49 +0530

    Add env types

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,10 +1,11 @@
 import arcjet, { detectBot, shield, slidingWindow } from '@arcjet/next'
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { env } from './data/env/server'
 
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/'])
 
 const aj = arcjet({
-  key: process.env.ARCJET_KEY!,
+  key: env.ARCJET_KEY,
   rules: [
     shield({ mode: "LIVE" }),
     detectBot({
