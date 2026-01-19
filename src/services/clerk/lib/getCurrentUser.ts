c4fa4955cd9ee5320fd8d49c979af5e0bf76a297
Sudeep <sudeeppprajapati@gmail.com>
2026-01-19 18:27:46 +0530

    Add auth helpers and user actions with caching

---
+++ src/services/clerk/lib/getCurrentUser.ts
@@ -0,0 +1,26 @@
+import { db } from "@/drizzle/db"
+import { UserTable } from "@/drizzle/schema"
+import { getUserIdTag } from "@/features/users/dbCache"
+import { auth } from "@clerk/nextjs/server"
+import { eq } from "drizzle-orm"
+import { cacheTag } from "next/cache"
+
+export async function getCurrentUser({ allData = false } = {}) {
+    const { userId, redirectToSignIn } = await auth()
+
+    return {
+        userId,
+        redirectToSignIn,
+        user: allData && userId != null ? await getUser(userId) : undefined
+    }
+}
+
+export async function getUser(id: string) {
+    "use cache"
+
+    cacheTag(getUserIdTag(id))
+
+    return db.query.UserTable.findFirst({
+        where: eq(UserTable.id, id)
+    })
+}
