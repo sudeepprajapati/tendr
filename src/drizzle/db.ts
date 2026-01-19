6eef4d6983d3e1b827facd1a514e22a0d23d4b06
Sudeep <sudeeppprajapati@gmail.com>
2026-01-19 18:21:28 +0530

    export db instance

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -3,4 +3,4 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 
 import * as schema from '@/drizzle/schema'
 
-const db = drizzle(env.DATABASE_URL, { schema });
+export const db = drizzle(env.DATABASE_URL, { schema });
